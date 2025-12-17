c0d278b92daea957a9257c94095544f58c6f9eb4
zikatron <batsi.ziki@bytefuse.ai>
2025-12-17 09:36:20 +0200

    chore: cleanup

--- eval_results.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-file_path = "/home/bytefuse/batsi/SB-MCL/experiments/gemcl/evaluation-10t10s-meta_test.pt"
-
-# Load the content
-loaded_data = torch.load(file_path)
-
-# You can then inspect or use the loaded_data
-print(loaded_data)
